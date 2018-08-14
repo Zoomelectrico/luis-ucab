ef89a9cb9ae5b4ac1a178400b186c0a662d2413e
Jose Roberto Quevedo <jose.quevedo2011@gmail.com>
2018-08-14 19:19:59 -0400

    arreglando el landing

--- src/app/components/client-db/client-db.component.ts
+++ src/app/components/client-db/client-db.component.ts
@@ -1,12 +1,24 @@
+/// <reference types="googlemaps" />
 import { Component, OnInit, ViewChild } from "@angular/core";
-import {} from "@types/googlemaps";
+
 @Component({
   selector: "app-client-db",
   templateUrl: "./client-db.component.html",
   styleUrls: ["./client-db.component.css"]
 })
 export class ClientDBComponent implements OnInit {
+  @ViewChild("gmap")
+  private gmapElement: any;
+  private map: google.maps.Map;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
